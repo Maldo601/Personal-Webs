5796dbc3d5e901bc3df166a9fad7a7ab2445ab50
Maldo <47955748+Maldo601@users.noreply.github.com>
2020-04-07 14:45:08 +0200

    Add files via upload

---
+++ Web/README.txt
@@ -0,0 +1,8 @@
+*** Pàgina destinada als grups de pràctiques *** 
+
+L'ús d'aquesta pot ser usat lliurement.  
+
+Autor: Joan Marc Maldonado
+Mail: maldo601@gmail.com
+
+
---
+++ Web/ping.php
@@ -0,0 +1,30 @@
+<?php
+$ip = $_REQUEST['ip'];
+
+if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $ip);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    // Petición HEAD
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+
+    $content = curl_exec($ch);
+
+    if (!curl_errno($ch)) {
+        $info = curl_getinfo($ch);
+
+        print_r("\nSe recibió respuesta " . $info['http_code'] . ' en ' . $info['total_time'] . " segundos \n");
+    } else {
+        print_r("\nError en petición: " . curl_error($ch) . "\n");
+    }
+
+    curl_close($ch);
+
+} else {
+    print_r("\nDirección IP no válida: " . $ip . "\n");
+}
